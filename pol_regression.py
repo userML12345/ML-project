816bdbe1fe38f1d553047c0ea7ba1109a8a6af99
userML12345 <101257201+userML12345@users.noreply.github.com>
2022-03-09 18:17:52 +0200

    Add files via upload

--- pol_regression.py
+++ pol_regression.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error
 
 
 df = pd.read_csv('coin_Bitcoin.csv')
@@ -15,36 +14,26 @@ X = rawData[['Close', 'Volume']].to_numpy()
 
 y = np.asarray(rawData['Marketcap']).reshape(-1, 1)
 
-X_train, X_rem, y_train, y_rem = train_test_split(X, y, test_size=0.3, random_state=42)
-X_val, X_test, y_val, y_test = train_test_split(X_rem, y_rem, test_size=0.4, random_state=42)
 
-print(len(X_test))
-print(len(X_val))
-print(len(X))
+cv = KFold(n_splits=10, random_state=42, shuffle=True)
 
+validation_errors = []
 
+for train_index, val_index in cv.split(y):
 
-lin_regr = LinearRegression()
+  X_train, X_val = X[train_index], X[val_index]
+  y_train, y_val = y[train_index], y[val_index]
 
-poly = PolynomialFeatures(degree=1)
+  print(len(X_val))
 
-X_train_poly = poly.fit_transform(X_train)
-lin_regr.fit(X_train_poly, y_train)
+  lin_regr = LinearRegression()
 
-y_pred_train = lin_regr.predict(X_train_poly)
-tr_error = mean_squared_error(y_train, y_pred_train)
+  lin_regr.fit(X_train, y_train)
 
-X_val_poly = poly.fit_transform(X_val)
-y_pred_val = lin_regr.predict(X_val_poly)
-val_error = mean_squared_error(y_val, y_pred_val)
-
-X_test_poly = poly.fit_transform(X_test)
-y_pred_test = lin_regr.predict(X_test_poly)
-test_error = mean_squared_error(y_test, y_pred_test)
+  y_pred_val = lin_regr.predict(X_val)
+  val_error = mean_squared_error(y_pred_val, y_val)
+  
+  validation_errors.append(val_error)
 
 
-print(tr_error)
-print(val_error)
-print(test_error)
-
-print(r2_score(y_test, y_pred_test))
+print(validation_errors)
